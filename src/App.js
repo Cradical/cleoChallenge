59c06fd2fccf9456ef4c4ba56ea7ab214cef1c24
Conrad Wright <easp.conrad@gmail.com>
2019-10-29 19:36:46 -0600

    Add reactstrap dependency

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const questionSet = [
+  {
+    prompt: 'Do you want to build a snowman?',
+    type: 'BOOLEAN',
+    index: 0,
+    options: ['yes', 'no'],
+    conditions: [],
+    answer: undefined,
+  },
+  {
+    prompt: 'Why not?',
+    type: 'SELECT_ONE',
+    index: 1,
+    options: ["I don't like snowmen", 'Other'],
+    conditions: [
+      {
+        chosenAnswerIndex: 0,
+        operator: '==',
+        answer: false,
+      },
+    ],
+    answer: undefined,
+  },
+  {
+    prompt: 'Do you want some snow?',
+    type: 'BOOLEAN',
+    index: 1,
+    options: ['yes', 'no'],
+    conditions: [
+      {
+        chosenAnswerIndex: 0,
+        operator: '==',
+        answer: true,
+      },
+    ],
+    answer: undefined,
+  },
+]
+
+const answer = []
+let currentIndex = 0
+
+const incrementIndex = () => {}
+
+const processAnswer = value => {
+  if (value === undefined) return
+
+  const currentQuestionSet = questionSet[currentIndex] // this needs to be cloned
+
+  currentQuestionSet.answer = value
+  answer.push(currentQuestionSet)
+
+  incrementIndex()
 }
 
-export default App;
+const handleBlur = event => {
+  console.log(event.target.value)
+  processAnswer(event.target.value)
+}
+
+const PromptQuestion = props => {
+  const { questions } = props
+
+  return (
+    <div>
+      <label>
+        {questions[0].prompt} (i.e. {questions[0].options.join(', ')})
+      </label>
+      <input type='text' onBlur={handleBlur} />
+    </div>
+  )
+}
+
+function App() {
+  console.log(answer)
+  console.log(currentIndex)
+  return (
+    <div className='App'>
+      <form>
+        <PromptQuestion questions={questionSet} />
+      </form>
+      <div>{currentIndex}</div>
+    </div>
+  )
+}
+
+export default App
